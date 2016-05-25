71780b416aaea6079072bfbc94d1ec47b82ae068
Thomas <evans_760@yahoo.com>
2016-05-25 14:50:44 -0700

    ,

---
+++ src/js/friends.js
@@ -0,0 +1,85 @@
+/*
+var friends = [
+                'Moe',
+                'Larry',
+                'Curly',
+                'Jane',
+                'Emma',
+                'Elizabeth',
+                'Elinor',
+                'Mary',
+                'Darcy',
+                'Grey',
+                'Lydia',
+                'Harriet'
+              ];
+
+              console.log(friends[friends.length -1]);
+
+              console.log(friends.sort());
+
+
+var friends = "Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet";
+
+arrayoffriends = (friends.split(","));
+arrayoffriends = arrayoffriends.sort();
+console.log(arrayoffriends.reverse());
+
+
+
+var myFriends = [
+                  'Rickon',
+                  'Meera',
+                  'Hodor',
+                  'Jojen',
+                  'Osha',
+                  'Rickard',
+                  'Maester',
+                  'Rodrik',
+                  'Jory',
+                  'Septa',
+                  'Jon'
+                ];
+
+var yourFriends = [
+                    'Bilbo',
+                    'Boromir',
+                    'Elrond',
+                    'Faramir',
+                    'Frodo',
+                    'Gandalf',
+                    'Legolas',
+                    'Pippin'
+                  ];
+
+                  myFriends = myFriends.concat(yourFriends);
+                  myFriends = myFriends.sort();
+                  console.log(myFriends);
+
+*/
+
+var foods = [
+              'Popcorn',
+              'Potato chips',
+              'Shrimp',
+              'Chicken rice',
+              'Poutine',
+              'Tacos',
+              'Toast',
+              'French Toast',
+              'Crab',
+              'Pho',
+              'Lasagna',
+              'Brownie',
+              'Lobster',
+              'Donuts',
+              'Ice cream',
+              'Hamburger',
+              'Sushi',
+              'Chocolate',
+              'Pizza'
+            ];
+
+console.log(foods.indexOf("Pho"));
+foods.splice(13, 1);
+console.log(foods);
